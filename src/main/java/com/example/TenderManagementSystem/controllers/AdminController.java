cfa18717747f6a4d689395dc54c80aa225b6f77e
Priyansh <129689746+Priyansh-Fanatic@users.noreply.github.com>
2025-02-10 23:16:47 +0530

    Update AdminController

--- src/main/java/com/example/TenderManagementSystem/controllers/AdminController.java
+++ src/main/java/com/example/TenderManagementSystem/controllers/AdminController.java
@@ -21,7 +21,7 @@ public class AdminController {
 
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/TenderManagementSystem";
     private static final String USER = "root";
-    private static final String DB_PASSWORD = "1471";
+    private static final String DB_PASSWORD = "PASSWORD";
 
     @GetMapping("/admin")
     public String adminPage(Model model) {
